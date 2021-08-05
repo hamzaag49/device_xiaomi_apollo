8f3a22d21da660e74392da6e4c47542a87fba74e
hamzaag49 <mhmdeveloperofficial@gmail.com>
2021-08-05 09:43:59 +0530

    Bring Up PE

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_apollo.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_apollo-user \
+    aosp_apollo-userdebug \
+    aosp_apollo-eng
---
+++ BoardConfig.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/apollo
+
+# Inherit from sm8250-common
+include device/xiaomi/sm8250-common/BoardConfigCommon.mk
+
+BUILD_BROKEN_DUP_RULES := true
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollo
+TARGET_RECOVERY_DEVICE_MODULES := libinit_apollo
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
+
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := apollon,apollo,apollopro
+
+# Inherit from the proprietary version
+include vendor/xiaomi/apollo/BoardConfigVendor.mk
