e05f58d9c5fbb71de9e1ab69490df6674f0143bf
hamzaag49 <mhmdeveloperofficial@gmail.com>
2021-08-06 18:52:14 +0530

    apollo: Bring Up PixysOS

--- pixys_apollo.mk
+++ pixys_apollo.mk
@@ -9,12 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixys/config/common_full_phone.mk)
 
 # Inherit from apollon device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-PRODUCT_NAME := aosp_apollo
+PRODUCT_NAME := pixys_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
@@ -25,6 +25,14 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 #Pixel offline charge animation
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
+#Enabled Carrier Settings
+TARGET_USES_CARRIERSETTINGS := true
+
+#Building TWRP By Dfault
+WITH_TWRP := true
+
+#Build with Gapps
+BUILD_WITH_GAPPS := true
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="apollo-user 11 RKQ1.200826.002 V12.5.1.0.RJDMIXM release-keys"
 
